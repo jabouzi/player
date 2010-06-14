45d7a375f2da8221413f1edffc685b7916f44546
Skander Jabouzi <jabouzi@gmail.com>
2010-06-14 00:57:41 +0000

    adding projet first time
    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/player/trunk@2 963ea74a-12ec-4e14-9696-0b619f887bd3

---
+++ src/player.h
@@ -0,0 +1,72 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+//
+#include <QMainWindow>
+#include <QtGui>
+#include <QDialog>
+#include <QString>
+#include <phonon>
+#include "ui_player.h"
+//
+class Player : public QMainWindow, public Ui::Player
+{
+Q_OBJECT
+public:
+    Player( QWidget * parent = 0, Qt::WFlags f = 0 );
+    
+public:
+    void loadFiles(QStringList);
+    void setTitle(QString);
+    
+private:
+    QList<Phonon::MediaSource> sources;
+    void setActions();
+    void setUI();
+    bool isplay;
+    QStringList audioSource;
+    QString audioLabel;
+    void adjustWindow();
+    int screenx;
+    int screeny;
+    bool timeLeft;
+    QString path;
+    int index;
+    bool stopped;
+    int playing;
+    bool newLoad;
+    void init();
+    void init2();
+    void closeEvent(QCloseEvent *);
+    void setLabel();
+    void setAudio(QStringList);
+    Phonon::MediaSource getAudio();    
+    QString calculateTime(int);
+    QDesktopWidget *desktop;
+    int screenWidth;
+    int width; 
+    int screenHeight;
+    int height;
+    int x;
+    int y;
+    QSize windowSize;
+
+private slots:
+    void finished();
+    void next();
+    void prev();
+    void load();
+    void play();
+    void stop();    
+    void autoPlay();
+    void updateTime();
+    void seekFile();
+    void stateChanged ( Phonon::State, Phonon::State );
+    void updateInfo();
+    void add();
+    void changeVolume();
+};
+#endif
+
+
+
+
